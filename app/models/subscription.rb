f7d882ab7a2f5afbfbd4c530080387c4138faa04
Mo Bitar <me@bitar.io>
2019-05-08 07:57:28 -0500

    Exception handling for weekly mailer

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -8,7 +8,13 @@ class Subscription < ApplicationRecord
     subscriptions = Subscription.where(:frequency => "weekly", :verified => true, :unsubscribed => false)
     subscriptions.each do |subscription|
       # deliver_later doesn't work for this for some reason as we're calling this from schedule.rb (runner)
-      SubscriptionMailer.weekly_digest(subscription.id).deliver_now
+      # there's an issue where subscription.last_mailing isn't being saved for some subs. I believe it's because
+      # there may be an exception triggering here causing subscriptions to not save.
+      begin
+        SubscriptionMailer.weekly_digest(subscription.id).deliver_now
+      rescue => e
+
+      end
       subscription.last_mailing = DateTime.now
     end
     subscriptions.each(&:save)
