63f06e609b226bbf264d61705a9625697957a067
Antonella Sgarlatta <antsgar@gmail.com>
2020-06-22 00:54:41 -0300

    Remove unnecessary (for now) Webpack configs

--- config/webpack/client.js
+++ config/webpack/client.js
@@ -1,10 +1,3 @@
 const environment = require('./environment');
-const devBuild = process.env.NODE_ENV === 'development';
-
-if (devBuild) {
-  environment.loaders
-    .get('sass')
-    .use.find((item) => item.loader === 'sass-loader').options.sourceMapContents = false;
-}
 
 module.exports = environment;
