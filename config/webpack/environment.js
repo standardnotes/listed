118abf67b8b5db20eaa7b0a49b10cf5f57d65d5a
Antonella Sgarlatta <antsgar@gmail.com>
2020-06-22 01:33:59 -0300

    Fixes in Webpacker and Webpack configs

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,6 +3,11 @@ const { environment } = require("@rails/webpacker");
 const rules = environment.loaders;
 const ManifestPlugin = environment.plugins.get("Manifest");
 
+rules.delete("file");
+rules.delete("css");
+rules.delete("sass");
+rules.delete("moduleCss");
+rules.delete("moduleSass");
 rules.delete("nodeModules");
 
 ManifestPlugin.options.writeToFileEmit = true;
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -5,11 +5,14 @@ const serverConf = require("./server");
 const merge = require("webpack-merge");
 
 const clientConfig = merge(clientEnvironment.toWebpackConfig(), {
-    mode: "development",
+    mode: "production",
 });
 
 const serverConfig = merge(serverConf, {
     mode: "production",
+    optimization: {
+        minimize: true,
+    },
 });
 
 module.exports = [clientConfig, serverConfig];
