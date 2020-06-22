66ba254267beb76e77f17079d130529ced716969
Antonella Sgarlatta <antsgar@gmail.com>
2020-06-22 00:33:07 -0300

    Migrate usage

--- client/app/packs/clientRegistration.js
+++ client/app/packs/clientRegistration.js
@@ -1,6 +1,8 @@
 import ReactOnRails from "react-on-rails";
 import AuthorHeader from "../components/authors/Header";
+import Usage from "../components/usage/Usage";
 
 ReactOnRails.register({
-    AuthorHeader
+    AuthorHeader,
+    Usage
 });
