27705bd5c451b44d67957c6d3775e0d75a392a27
Antonella Sgarlatta <antsgar@gmail.com>
2020-06-27 21:45:01 -0300

    Migrate author page

--- client/app/startup/serverRegistration.js
+++ client/app/startup/serverRegistration.js
@@ -1,10 +1,12 @@
 import ReactOnRails from "react-on-rails";
 import AuthorHeader from "../components/authors/Header";
+import AuthorShow from "../components/authors/Show";
 import SharedFooter from "../components/shared/Footer";
 import Usage from "../components/usage/Usage";
 
 ReactOnRails.register({
     AuthorHeader,
+    AuthorShow,
     SharedFooter,
     Usage
 });
