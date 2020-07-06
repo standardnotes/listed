35eeadcd0817f5b1bd65742a2c30cbe2c7a8a2b3
Antonella Sgarlatta <antsgar@gmail.com>
2020-07-06 01:29:06 -0300

    Fix turbolinks

--- client/app/packs/clientRegistration.js
+++ client/app/packs/clientRegistration.js
@@ -1,10 +1,13 @@
 import ReactOnRails from "react-on-rails";
+import Turbolinks from "turbolinks";
 import AuthorHeader from "../components/authors/Header";
 import AuthorShow from "../components/authors/Show";
 import SharedFooter from "../components/shared/Footer";
 import Usage from "../components/usage/Usage";
 import PostShow from "../components/posts/Show";
 import SubscriptionValidate from "../components/subscriptions/Validate";
+
+Turbolinks.start();
 
 ReactOnRails.register({
     AuthorHeader,
@@ -13,4 +16,8 @@ ReactOnRails.register({
     Usage,
     PostShow,
     SubscriptionValidate
+});
+
+ReactOnRails.setOptions({
+    traceTurbolinks: TRACE_TURBOLINKS
 });
