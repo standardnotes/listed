b7b36ef36a1bec23e8d5af27ac8e032965df4995
Antonella Sgarlatta <antsgar@gmail.com>
2020-07-12 20:38:58 -0300

    Migrate Subscribe page

--- client/app/packs/clientRegistration.js
+++ client/app/packs/clientRegistration.js
@@ -2,6 +2,7 @@ import ReactOnRails from "react-on-rails";
 import Turbolinks from "turbolinks";
 import AuthorHeader from "../components/authors/Header";
 import AuthorShow from "../components/authors/Show";
+import AuthorSubscribe from "../components/authors/Subscribe";
 import SharedFooter from "../components/shared/Footer";
 import Usage from "../components/usage/Usage";
 import PostShow from "../components/posts/Show";
@@ -12,6 +13,7 @@ Turbolinks.start();
 ReactOnRails.register({
     AuthorHeader,
     AuthorShow,
+    AuthorSubscribe,
     SharedFooter,
     Usage,
     PostShow,
--- client/app/startup/serverRegistration.js
+++ client/app/startup/serverRegistration.js
@@ -1,6 +1,7 @@
 import ReactOnRails from "react-on-rails";
 import AuthorHeader from "../components/authors/Header";
 import AuthorShow from "../components/authors/Show";
+import AuthorSubscribe from "../components/authors/Subscribe";
 import SharedFooter from "../components/shared/Footer";
 import Usage from "../components/usage/Usage";
 import PostShow from "../components/posts/Show";
@@ -9,6 +10,7 @@ import SubscriptionValidate from "../components/subscriptions/Validate";
 ReactOnRails.register({
     AuthorHeader,
     AuthorShow,
+    AuthorSubscribe,
     SharedFooter,
     Usage,
     PostShow,
