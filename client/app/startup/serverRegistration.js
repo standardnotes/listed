8de8d5756c62b00776f0500f464aa0f2dd687c05
Antonella Sgarlatta <antsgar@gmail.com>
2020-07-12 22:31:34 -0300

    Migrate Thank page

--- client/app/startup/serverRegistration.js
+++ client/app/startup/serverRegistration.js
@@ -2,6 +2,7 @@ import ReactOnRails from "react-on-rails";
 import AuthorHeader from "../components/authors/Header";
 import AuthorShow from "../components/authors/Show";
 import AuthorSubscribe from "../components/authors/Subscribe";
+import AuthorTip from "../components/authors/Tip";
 import SharedFooter from "../components/shared/Footer";
 import Usage from "../components/usage/Usage";
 import PostShow from "../components/posts/Show";
@@ -11,6 +12,7 @@ ReactOnRails.register({
     AuthorHeader,
     AuthorShow,
     AuthorSubscribe,
+    AuthorTip,
     SharedFooter,
     Usage,
     PostShow,
