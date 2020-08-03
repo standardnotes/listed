c123d6a2a1f16f48c37a5ca73326769328842b7c
Antonella Sgarlatta <antsgar@gmail.com>
2020-08-03 01:10:44 -0300

    Migrate settings guestbook entries

--- client/app/components/authors/Settings.jsx
+++ client/app/components/authors/Settings.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import EditAuthor from "./settings/EditAuthor";
 import PaymentDetails from "./settings/PaymentDetails";
 import CustomDomain from "./settings/CustomDomain";
+import GuestbookEntries from "./settings/GuestbookEntries";
 
-export default ({ author, authenticityToken, customDomainIP }) => {
+export default ({ author, authenticityToken, customDomainIP, guestbookEntries }) => {
     return (
         <div id="settings" className="sn-component single-post-show">
             <h3>Settings</h3>
@@ -22,6 +23,8 @@ export default ({ author, authenticityToken, customDomainIP }) => {
             <EditAuthor author={author} authenticityToken={authenticityToken} />
             <PaymentDetails author={author} />
             <CustomDomain author={author} authenticityToken={authenticityToken} customDomainIP={customDomainIP} />
+            <hr className="mt-30" />
+            <GuestbookEntries guestbookEntries={guestbookEntries} />
         </div>
     );
 };
---
+++ client/app/components/authors/settings/GuestbookEntries.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import moment from "moment";
+import axios from "axios";
+
+export default ({ guestbookEntries }) => {
+    const handleEntryAction = (url) => {
+        axios
+            .get(url)
+            .then(response => {
+                window.location.href = response.request.responseURL;
+            })
+    }
+
+    return(
+        <div>
+            <h3>Guestbook Entries</h3>
+            <div className="guestbook sk-panel static">
+                {guestbookEntries.length == 0 && (
+                    <p>
+                        No guestbook entries yet.
+                    </p>
+                )}
+                {guestbookEntries.map(entry => (
+                    <div key={entry.id} className="sk-notification contrast">
+                        <p className="sk-p">
+                            <strong>{entry.text}</strong>
+                        </p>
+                        <i className="sk-p">
+                            {moment(entry.created_at).format("MMMM D, YYYY")}
+                        </i>
+                        {entry.signer_email && entry.donation_info.length > 0 && (
+                            <p className="sk-p">
+                                Author email: {entry.signer_email}
+                            </p>
+                        )}
+                        {entry.donation_info && entry.donation_info.length > 0 && (
+                            <p className="sk-p">
+                                Donation info: {entry.donation_info}
+                            </p>
+                        )}
+                        <div className="sk-horizontal-group mt-10">
+                            {entry.public ? (
+                                <a className="sk-p" href="#" onClick={e => handleEntryAction(entry.unapproval_url)}>
+                                    Unpublish
+                                </a>
+                            ) : (
+                                <a className="sk-p" href="#" onClick={e => handleEntryAction(entry.approval_url)}>
+                                    Publish
+                                </a>
+                            )}
+                            <a className="sk-p" href="#" onClick={e => handleEntryAction(entry.spam_url)}>
+                                Report Spam
+                            </a>
+                            <a className="sk-p" href="#" onClick={e => handleEntryAction(entry.deletion_url)}>
+                                Delete
+                            </a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
