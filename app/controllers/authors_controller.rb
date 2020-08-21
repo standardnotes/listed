6ea6bda1ef593400a33b21e371814281ce77251e
Karol Sójko <karolsojko@gmail.com>
2020-08-21 14:05:23 +0200

    tmp: disable creating ssl certificates for performance check

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -257,9 +257,9 @@ class AuthorsController < ApplicationController
       @author.domain = Domain.new
     end
 
-    certificate = SSLCertificate.find_or_create_by(domain: params[:domain])
-    certificate.key = ENV['LETSENCRYPT_PRIVATE_KEY']
-    certificate.save
+    # certificate = SSLCertificate.find_or_create_by(domain: params[:domain])
+    # certificate.key = ENV['LETSENCRYPT_PRIVATE_KEY']
+    # certificate.save
 
     @author.domain.domain = params[:domain]
     @author.domain.extended_email = params[:extended_email]
