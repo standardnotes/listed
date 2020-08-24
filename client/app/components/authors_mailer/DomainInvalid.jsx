c275a708688a80e147c86e4559f92866771003cd
Karol Sójko <karolsojko@gmail.com>
2020-08-24 08:38:32 +0200

    fix: add custom domain ip from env vars to invalid domain email

--- client/app/components/authors_mailer/DomainInvalid.jsx
+++ client/app/components/authors_mailer/DomainInvalid.jsx
@@ -6,7 +6,7 @@ export default () => {
             <p>
                 Hi there, your Listed domain settings were not configured properly.
                 Please make sure you have an A record pointing to
-                <strong> 18.205.249.107</strong>,
+                <strong> {customDomainIP}</strong>,
                 then
                 <strong> submit your domain request again via your author settings</strong>.
                 You'll know you have it configured correctly if when you visit your custom domain, it shows an
