59673fda70850e7fc15b779a7db943ffc1ebeef9
Karol Sójko <karolsojko@gmail.com>
2020-08-24 16:32:23 +0200

    fix: saving ssl certificate

--- app/jobs/ssl_certificate_create_job.rb
+++ app/jobs/ssl_certificate_create_job.rb
@@ -1,6 +1,5 @@
 class SslCertificateCreateJob < ApplicationJob
   def perform(domain)
-    certificate = SSLCertificate.find_or_create_by(domain: domain)
-    certificate.save
+    SSLCertificate.find_or_create_by(domain: domain)
   end
 end
