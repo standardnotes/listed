9cfb64a6c1a8cb3d579be98b11e7ff08e4c7321b
Karol Sójko <karolsojko@gmail.com>
2020-08-24 11:02:39 +0200

    fix: path to server lock pid

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,7 @@ COMMAND=$1 && shift 1
 case "$COMMAND" in
   'start' )
     echo "Prestart Step 1/2 - Removing server lock"
-    rm -f /app/tmp/pids/server.pid
+    rm -f /listed/tmp/pids/server.pid
     echo "Prestart Step 2/2 - Migrating database"
     bundle exec rake db:migrate:ignore_concurrent
     echo "Starting Server..."
@@ -15,7 +15,7 @@ case "$COMMAND" in
 
   'renew-certificates' )
     echo "Prestart Step 1/2 - Removing server lock"
-    rm -f /app/tmp/pids/server.pid
+    rm -f /listed/tmp/pids/server.pid
     echo "Prestart Step 2/2 - Renew SSL certificates"
     if [[ -z "$LETSENCRYPT_AWS_NETWORK_LOAD_BALANCER_LISTENER_ARN" ]]; then
       echo "Skipped renewing SSL certificates - LETSENCRYPT_AWS_NETWORK_LOAD_BALANCER_LISTENER_ARN environment variable not set"
