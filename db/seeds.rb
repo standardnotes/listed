a0df899ff5fd66a7ffcacf3c99ddf49104540f13
Karol Sójko <karolsojko@gmail.com>
2020-08-25 10:50:00 +0200

    fix: creating ssl certificates in db seed step

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 existing_domains = Domain.all
 
 existing_domains.each do |existing_domain|
-  LetsEncrypt.certificate_model.find_or_create_by(domain: existing_domain.domain)
+  SSLCertificate.find_or_create_by(domain: existing_domain.domain)
 end
