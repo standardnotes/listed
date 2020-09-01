2d86b45a584f68d0020c932a56f612a5b7c6362b
Karol Sójko <karolsojko@gmail.com>
2020-09-01 13:06:06 +0200

    fix: remove .env file from deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :git_strategy, Capistrano::Git::SubmoduleStrategy
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_files, fetch(:linked_files, []).push('.env')
+# set :linked_files, fetch(:linked_files, []).push('.env')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
