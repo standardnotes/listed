86e409118bccab08a9f92b7850ea8e068bef7cc1
Karol Sójko <karolsojko@gmail.com>
2020-09-04 14:19:27 +0200

    fix: cleanup for pr

--- db/seed.rb
+++ db/seed.rb
@@ -1,10 +1,13 @@
-APP_ROOT = File.expand_path("..", __dir__)
+require 'fileutils'
 
 certificates = LetsEncrypt.certificate_model.all
 
 certificates.each do |certificate|
   if certificate.intermediaries.present? && certificate.key.present?
-    File.open("#{APP_ROOT}/certificates/#{certificate.domain}/fullchain.pem", 'w+') { |file| file.write(certificate.intermediaries) }
-    File.open("#{APP_ROOT}/certificates/#{certificate.domain}/privkey.pem", 'w+') { |file| file.write(certificate.key) }
+    certificate_directory = "#{ENV['CERTIFICATES_FOLDER_PATH']}/#{certificate.domain}"
+    FileUtils.mkdir_p certificate_directory unless File.exists?(certificate_directory)
+
+    File.open("#{certificate_directory}/fullchain.pem", 'w+') { |file| file.write(certificate.intermediaries) }
+    File.open("#{certificate_directory}/privkey.pem", 'w+') { |file| file.write(certificate.key) }
   end
 end
