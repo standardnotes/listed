e5226c0eaecc616cc19cede0b2243e685cab2c34
Karol Sójko <karolsojko@gmail.com>
2020-09-04 14:32:39 +0200

    fix: workflow setup for develop

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -9,7 +9,7 @@
 
 server ENV.fetch('LISTED_SSH_SERVER'), user: ENV.fetch('LISTED_SSH_USER'), roles: %w{app db web}
 
-set :branch, 'domainer_renewal'
+set :branch, 'develop'
 
 set :deploy_to, ENV.fetch('LISTED_SSH_DEPLOY_TO')
 
