ee8cede671d85aa141b1535d171226a0f075f913
Antonella Sgarlatta <antsgar@gmail.com>
2020-09-20 15:53:56 -0300

    Add card style to reusable styles

--- client/app/components/usage/active_authors/ActiveAuthorItem.jsx
+++ client/app/components/usage/active_authors/ActiveAuthorItem.jsx
@@ -5,34 +5,36 @@ import "./ActiveAuthorItem.scss";
 
 const ActiveAuthorItem = ({ author }) => {
     return(
-        <a className="author active-author" href={author.url}>
-            <div className="active-author__title">
-                <h5 className="h5">
-                    {author.title}
-                </h5>
-                {author.featured && 
-                    <SVG src={IcStarCircleFilled} className="active-author__icon active-author__icon--featured" />
+        <div className="card author active-author">
+            <a href={author.url}>
+                <div className="active-author__title">
+                    <h5 className="h5">
+                        {author.title}
+                    </h5>
+                    {author.featured && 
+                        <SVG src={IcStarCircleFilled} className="active-author__icon active-author__icon--featured" />
+                    }
+                </div>
+                {author.bio &&
+                    <p className="bio active-author__bio p2">{author.bio}</p>
                 }
-            </div>
-            {author.bio &&
-                <p className="bio active-author__bio p2">{author.bio}</p>
-            }
-            {author.easterEgg ? (
-                <div className="easter-egg__sign-up">
-                    <p className="p3">
-                        Sign up for Listed
-                    </p>
-                    <SVG src={IcArrowLong} className="easter-egg__icon--arrow" />
-                </div>
-            ) : (
-                <div className="active-author__word-count">
-                    <SVG src={IcTextRich} className="active-author__icon active-author__icon--word-count" />
-                    <p className="p3 active-author__word-count" suppressHydrationWarning>
-                        {`${author.word_count.toLocaleString()} words`}
-                    </p>
-                </div>
-            )}
-        </a>
+                {author.easterEgg ? (
+                    <div className="easter-egg__sign-up">
+                        <p className="p3">
+                            Sign up for Listed
+                        </p>
+                        <SVG src={IcArrowLong} className="easter-egg__icon--arrow" />
+                    </div>
+                ) : (
+                    <div className="active-author__word-count">
+                        <SVG src={IcTextRich} className="active-author__icon active-author__icon--word-count" />
+                        <p className="p3 active-author__word-count" suppressHydrationWarning>
+                            {`${author.word_count.toLocaleString()} words`}
+                        </p>
+                    </div>
+                )}
+            </a>
+        </div>
     );
 };
 
