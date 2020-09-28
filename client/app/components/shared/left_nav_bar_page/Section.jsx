6d5640aaf2fcd4a5d27b348b156bebf86f147ded
Antonella Sgarlatta <antsgar@gmail.com>
2020-09-28 00:13:41 -0300

    Use Javascript for collapsing/expanding sections

--- client/app/components/shared/left_nav_bar_page/Section.jsx
+++ client/app/components/shared/left_nav_bar_page/Section.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SVG from "react-inlinesvg";
 import { IcChevronDown, IcChevronUp } from "../../../assets/icons";
 import "./Section.scss";
@@ -8,13 +8,26 @@ const Section = ({ section }) => {
     const content = renderContent();
     const [isCollapsed, setIsCollapsed] = useState(collapsed);
 
+    useEffect(() => {
+        const sectionContentElement = document.querySelector(`#${section.id} .section__content`);
+
+        if (isCollapsed) {
+            sectionContentElement.style.height = "0px";
+            sectionContentElement.style.visibility = "hidden";
+        } else {
+            const sectionHeight = sectionContentElement.scrollHeight;
+            sectionContentElement.style.height = `${sectionHeight}px`;
+            sectionContentElement.style.visibility = "visible";
+        }
+    }, [isCollapsed]);
+
     return(
         <li className="section__container">
             <div id={id} className="card section--desktop">
                 <h3 className="h3">
                     {title}
                 </h3>
-                <div className="section__content--visible">
+                <div>
                     {content}
                 </div>
             </div>
@@ -30,7 +43,7 @@ const Section = ({ section }) => {
                         onClick={() => setIsCollapsed(!isCollapsed)}
                     />
                 </div>
-                <div className={`section__content ${isCollapsed ? "" : "section__content--visible"}`}>
+                <div className="section__content">
                     {content}
                 </div>
             </div>
