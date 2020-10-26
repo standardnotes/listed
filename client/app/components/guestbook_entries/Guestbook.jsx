63ec33d96e177b968ffbcd6a617a0fd901b274d3
Antonella Sgarlatta <antsgar@gmail.com>
2020-10-25 21:39:32 -0300

    Implement guestbook entries redesign

--- client/app/components/guestbook_entries/Guestbook.jsx
+++ client/app/components/guestbook_entries/Guestbook.jsx
@@ -1,24 +1,32 @@
 import React from "react";
+import "./Guestbook.scss";
 
 export default ({ sent, author, entries, newAuthorGuestbookEntryUrl }) => {
     return(
-        <div className="single-post-show">
-            {sent && (
-                <p>
-                    <strong>
-                        Thanks! Your message has been sent to the author. They'll need to confirm it before it appears in the public guestbook.
-                    </strong>
-                </p>
+        <div className="page-container guestbook__container">
+            <h1 className="h1">
+                Guestbook
+            </h1>
+            {!sent && (
+                <div className="guestbook__new-entry-button-container">
+                    <a className="button button--primary" href={newAuthorGuestbookEntryUrl}>New entry</a>
+                    {entries.length > 0 ? (
+                        <p className="p1">Write something in {author.title}'s guestbook!</p>
+                    ) : (
+                        <p className="p1">Be the first to sign {author.title}'s guestbook!</p>
+                    )}
+                </div>
             )}
-            <h3>
-                <a href={author.url}>{author.title}</a>'s Guestbook
-            </h3>
+            {sent && (
+                <div className="callout callout--success">
+                    Your message has been sent to the author. They'll need to confirm it before it appears in the public guestbook.
+                </div>
+            )}
             {entries.map(entry => (
-                <p key={entry.id}>
-                    <i>{entry.text}</i>
+                <p key={entry.id} className="p1 guestbook__entry">
+                    {entry.text}
                 </p>
             ))}
-            <a href={newAuthorGuestbookEntryUrl}>Sign {author.title}'s guestbook</a>
         </div>
     );
 };
