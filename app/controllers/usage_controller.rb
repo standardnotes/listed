de9ab0df743ba87010290547f1020b34cb746899
Karol Sójko <karolsojko@gmail.com>
2020-12-29 09:44:34 +0100

    fix: easter egg index when no authors available

--- app/controllers/usage_controller.rb
+++ app/controllers/usage_controller.rb
@@ -1,17 +1,16 @@
 class UsageController < ApplicationController
-  before_action {
-    set_meta_images_for_letter("L")
-  }
+  before_action do
+    set_meta_images_for_letter('L')
+  end
 
   def index
-    # @hide_header = true
     active_authors = Author.active_authors
-    easter_egg_index = rand(1..active_authors.size)
+    easter_egg_index = active_authors.size.positive? ? rand(1..active_authors.size) : 0
     easter_egg = {
-      :id => "easter-egg",
-      :title => "This could be you :)",
-      :bio => "Share your experience in its truest form. Start writing now.",
-      :featured => false
+      id: 'easter-egg',
+      title: 'This could be you :)',
+      bio: 'Share your experience in its truest form. Start writing now.',
+      featured: false
     }
     active_authors.insert(easter_egg_index, easter_egg)
     @active_authors = active_authors
@@ -21,7 +20,7 @@ class UsageController < ApplicationController
     if params[:secret_url]
       @secret_url = Base64.encode64(params[:secret_url])
     else
-      redirect_to "/"
+      redirect_to '/'
     end
   end
 end
