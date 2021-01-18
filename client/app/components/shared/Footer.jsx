1ee302c60f34ed1762880cf5c2b82170b398254c
Antonella Sgarlatta <antsgar@gmail.com>
2021-01-17 21:23:58 -0300

    Copyright adjustments

--- client/app/components/shared/Footer.jsx
+++ client/app/components/shared/Footer.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Footer.scss";
 
-const Footer = ({ blogPage }) => {
+const Footer = ({ blogPage, author }) => {
     return (
         <div id="footer" className={blogPage ? "footer--blog-page" : ""}>
             <div className="footer__container">
                 <p className="p3">Listed Blogging Platform</p>
-                <p className="p3">Copyright Ⓒ 2020</p>
+                {author && (
+                    <p className="p3">
+                        Copyright © {(new Date()).getFullYear()} {author.display_name}
+                    </p>
+                )}
                 <p className="p3">By{" "}
                     <a href="https://standardnotes.org" target="_blank" rel="noopener noreferrer">
                         Standard Notes
