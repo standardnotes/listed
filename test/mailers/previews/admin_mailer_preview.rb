6c9793174219d3ca5aa2bb0d3f246bf6139d937b
Antonella Sgarlatta <antsgar@gmail.com>
2021-03-02 10:28:34 -0300

    fix: fix port in mailer preview files

--- test/mailers/previews/admin_mailer_preview.rb
+++ test/mailers/previews/admin_mailer_preview.rb
@@ -1,4 +1,4 @@
-# Preview all emails at http://localhost:3000/rails/mailers/admin_mailer
+# Preview all emails at http://localhost:3009/rails/mailers/admin_mailer
 class AdminMailerPreview < ActionMailer::Preview
 
   def new_domain_request
