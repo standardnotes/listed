7a96008ed64122fec0a4df875fa3035209c131d0
Mo Bitar <me@bitar.io>
2021-03-27 16:44:23 -0500

    fix: reinstate migration

--- db/migrate/20210326223431_add_author_show_to_posts.rb
+++ db/migrate/20210326223431_add_author_show_to_posts.rb
@@ -1,21 +1,12 @@
 class AddAuthorShowToPosts < ActiveRecord::Migration[5.0]
   def change
-    # add_column :posts, :author_show, :boolean, default: false
-    # add_index :posts, :author_show
-    # add_index :posts, [:author_show, :created_at]
+    add_column :posts, :author_show, :boolean, default: false
+    add_index :posts, :author_show
+    add_index :posts, [:author_show, :created_at]
 
-    # add_column :posts, :author_page, :boolean, default: false
-    # add_index :posts, :author_page
+    add_column :posts, :author_page, :boolean, default: false
+    add_index :posts, :author_page
 
-    # add_column :authors, :css, :text, limit: 1.megabytes
-
-    Post.all.each do |post|
-      if post.metatype == 'css' && post.published
-        post.author&.update_css(post.text)
-      else
-        post.update_author_show_status
-        post.save
-      end
-    end
+    add_column :authors, :css, :text, limit: 1.megabytes
   end
 end
