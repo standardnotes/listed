19195b5ef6a376e6c51ad2834010501b3c1423f5
Mo Bitar <me@bitar.io>
2021-03-26 19:03:16 -0500

    tmp: combine migrations to allow deployment

--- db/migrate/20210326223431_add_author_show_to_posts.rb
+++ db/migrate/20210326223431_add_author_show_to_posts.rb
@@ -1,15 +1,21 @@
 class AddAuthorShowToPosts < ActiveRecord::Migration[5.0]
   def change
-    add_column :posts, :author_show, :boolean, default: false
-    add_index :posts, :author_show
-    add_index :posts, [:author_show, :created_at]
+    # add_column :posts, :author_show, :boolean, default: false
+    # add_index :posts, :author_show
+    # add_index :posts, [:author_show, :created_at]
 
-    add_column :posts, :author_page, :boolean, default: false
-    add_index :posts, :author_page
+    # add_column :posts, :author_page, :boolean, default: false
+    # add_index :posts, :author_page
+
+    add_column :authors, :css, :text, limit: 1.megabytes
 
     Post.all.each do |post|
-      post.update_author_show_status
-      post.save
+      if post.metatype == 'css' && post.published
+        post.author.update_css(post.text)
+      else
+        post.update_author_show_status
+        post.save
+      end
     end
   end
 end
--- db/migrate/20210326232803_add_theme_to_authors.rb
+++
@@ -1,9 +0,0 @@
-class AddThemeToAuthors < ActiveRecord::Migration[5.0]
-  def change
-    add_column :authors, :css, :text, limit: 1.megabytes
-
-    Post.where(metatype: 'css', published: true).each do |post|
-      post.author.update_css(post.text)
-    end
-  end
-end
