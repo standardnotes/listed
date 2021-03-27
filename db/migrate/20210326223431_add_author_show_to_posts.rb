b76aca13e232f776ba66ed8a96bddd9ad771e478
Mo Bitar <me@bitar.io>
2021-03-26 19:07:16 -0500

    fix: safety check for post.author

--- db/migrate/20210326223431_add_author_show_to_posts.rb
+++ db/migrate/20210326223431_add_author_show_to_posts.rb
@@ -7,11 +7,11 @@ class AddAuthorShowToPosts < ActiveRecord::Migration[5.0]
     # add_column :posts, :author_page, :boolean, default: false
     # add_index :posts, :author_page
 
-    add_column :authors, :css, :text, limit: 1.megabytes
+    # add_column :authors, :css, :text, limit: 1.megabytes
 
     Post.all.each do |post|
       if post.metatype == 'css' && post.published
-        post.author.update_css(post.text)
+        post.author&.update_css(post.text)
       else
         post.update_author_show_status
         post.save
