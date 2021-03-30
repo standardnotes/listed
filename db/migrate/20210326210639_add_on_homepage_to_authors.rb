35b1f5f542ad382d2d37b3a91763ea4a82ac2481
Mo Bitar <me@bitar.io>
2021-03-30 08:12:40 -0500

    fix: remove model based migration snippet

--- db/migrate/20210326210639_add_on_homepage_to_authors.rb
+++ db/migrate/20210326210639_add_on_homepage_to_authors.rb
@@ -2,14 +2,5 @@ class AddOnHomepageToAuthors < ActiveRecord::Migration[5.0]
   def change
     add_column :authors, :homepage_activity, :datetime
     add_index :authors, :homepage_activity
-
-    Author.all.each do |author|
-      if author.featured && !author.homepage_activity
-        author.homepage_activity = DateTime.now
-        author.save
-      else
-        author.update_homepage_status
-      end
-    end
   end
 end
