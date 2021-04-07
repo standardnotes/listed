a23123375fcb9487de4026ad3cff827a945a5507
Mo Bitar <me@bitar.io>
2021-04-07 08:32:34 -0500

    fix: homepage layout issue by enabling server rendering

--- app/controllers/usage_controller.rb
+++ app/controllers/usage_controller.rb
@@ -1,19 +1,20 @@
-class UsageController < ApplicationController
+# frozen_string_literal: true
 
+class UsageController < ApplicationController
   def index
     active_authors = Author
                      .includes(:domain)
                      .where.not(homepage_activity: nil)
                      .order(homepage_activity: :desc)
                      .to_a
-    # easter_egg_index = 5
-    # easter_egg = {
-    #   id: 'easter-egg',
-    #   title: 'This could be you :)',
-    #   bio: 'Share your experience in its truest form. Start writing now.',
-    #   featured: false
-    # }
-    # active_authors.insert(easter_egg_index, easter_egg)
+    easter_egg_index = active_authors.size.positive? ? rand(1..active_authors.size) : 0
+    easter_egg = {
+      id: 'easter-egg',
+      title: 'This could be you :)',
+      bio: 'Share your experience in its truest form. Start writing now.',
+      featured: false
+    }
+    active_authors.insert(easter_egg_index, easter_egg)
     @active_authors = active_authors
   end
 
