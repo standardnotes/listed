2244cf2285da9e11c8183820fd2a33da1b7c2e5a
Mo Bitar <me@bitar.io>
2021-04-07 07:47:48 -0500

    tmp: disable easter egg affecting homepage layout

--- db/migrate/20210405134917_perform_launch_prod_migration.rb
+++ db/migrate/20210405134917_perform_launch_prod_migration.rb
@@ -1,12 +1,12 @@
 class PerformLaunchProdMigration < ActiveRecord::Migration[5.0]
   def change
-    Post.where(unlisted: [false, nil], hidden: [false, nil], published: true, metatype: nil).each do |post|
+    Post.where(unlisted: [false, nil], hidden: [false, nil], published: true, metatype: nil).find_each do |post|
       post.author_show = !post.page
       post.author_page = post.page
       post.save
     end
 
-    Post.where(metatype: 'css', published: true).each do |post|
+    Post.where(metatype: 'css', published: true).find_each do |post|
       if post.author
         post.author.update_css(post.text)
         post.author.custom_theme_enabled = false
@@ -14,7 +14,7 @@ class PerformLaunchProdMigration < ActiveRecord::Migration[5.0]
       end
     end
 
-    Author.all.each do |author|
+    Author.all.find_each do |author|
       if author.featured && !author.homepage_activity
         author.homepage_activity = DateTime.now
         author.save
