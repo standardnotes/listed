91504def3b60e3fdd320951d22f7ccde9fdd9f4e
Mo <mo@standardnotes.com>
2021-12-28 12:21:23 -0600

    feat: secure cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_listed_session'
+Rails.application.config.session_store :cookie_store,
+  key: '_listed_session',
+  secret: ENV["SECRET_KEY_BASE"],
+  secure: Rails.env.production?
