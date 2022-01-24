e2d50579e83d47685eb91029e56648a9696160a9
Karol Sójko <karolsojko@protonmail.com>
2022-01-24 10:01:39 +0100

    fix: default from email to email@listed.to

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   add_template_helper(ReactOnRailsHelper)
-  default from: 'Listed <listed@standardnotes.org>',
+  default from: 'Listed <email@listed.to>',
           reply_to: 'help@standardnotes.com'
   layout 'mailer'
 end
