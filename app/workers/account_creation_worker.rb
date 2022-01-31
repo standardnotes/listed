6ec785289705dd5b5260e7ab8535819ba2a8d46d
Karol Sójko <karolsojko@protonmail.com>
2022-01-31 12:13:32 +0100

    fix: configure shoryuken capistrano requirements for workers folder

--- app/workers/account_creation_worker.rb
+++ app/workers/account_creation_worker.rb
@@ -1,7 +1,7 @@
 class AccountCreationWorker
   include Shoryuken::Worker
 
-  shoryuken_options queue: ->{ ENV['SQS_QUEUE_NAME'] }, auto_delete: true
+  shoryuken_options queue: ENV.fetch['SQS_QUEUE_NAME'], auto_delete: true
 
   def perform(sqs_msg, body)
     Rails.logger.info("Received SQS Message: #{sqs_msg}")
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -18,6 +18,7 @@ set :rails_env, 'production'
 set :keep_releases, 2
 
 set :shoryuken_queues, [ENV.fetch('SQS_QUEUE_NAME')]
+set :shoryuken_requires, ['./app/workers']
 
 # role-based syntax
 # ==================
